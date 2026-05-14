673fb0441fe9e68e8abb55832b04f57876f36d94
Lucas Almeida <lalmeida464@gmail.com>
2026-05-14 18:11:53 -0300

    First commit

---
+++ src/main/java/com/lalmeida/cobitmaturityapi/CobitMaturityApiApplication.java
@@ -0,0 +1,12 @@
+package com.lalmeida.cobitmaturityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobitMaturityApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CobitMaturityApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/lalmeida/cobitmaturityapi/CobitMaturityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lalmeida.cobitmaturityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobitMaturityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
